259ab0fb2027dc57f635b0629aa4e7d3ed990d6f
赤川大空 <daisuke1024akagawa@gmail.com>
2020-11-15 21:49:36 +0900

    chore

---
+++ GACS/GACS.Population/Population.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GACS
+{
+    public class Indivisual {
+    }
+
+    public class Population
+    {
+        public int PopulationSize;
+        public List<Indivisual> Indivisual;
+
+        public Population(int populationSize)
+        {
+            PopulationSize = populationSize;
+            InitializePopulation();
+        }
+
+        private void InitializePopulation()
+        {
+            this.Indivisual = new List<Indivisual>();
+            for (int i=0; i< PopulationSize; i++)
+            {
+                this.Indivisual.Add(new Indivisual());
+            }
+        }
+
+        public Indivisual this[int i]
+        {
+            get { return this.Indivisual[i]; }
+        }
+    }
+}
